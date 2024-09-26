d863dd09b2f95e374c3a27581852c1283d0cef4e
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2024-09-25 23:32:54 -0300

    borrado

--- app/src/main/java/com/rafalopez/recuperatorio/ui/borrar/BorrarFragment.java
+++ app/src/main/java/com/rafalopez/recuperatorio/ui/borrar/BorrarFragment.java
@@ -1,20 +1,27 @@
 package com.rafalopez.recuperatorio.ui.borrar;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.rafalopez.recuperatorio.R;
 import com.rafalopez.recuperatorio.databinding.FragmentBorrarBinding;
 import com.rafalopez.recuperatorio.databinding.FragmentGalleryBinding;
+import com.rafalopez.recuperatorio.entity.Inmueble;
 
 public class BorrarFragment extends Fragment {
 
@@ -30,12 +37,27 @@ public class BorrarFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mViewModel = new ViewModelProvider(this).get(BorrarViewModel.class);
+        binding = FragmentBorrarBinding.inflate(inflater,container,false);
         View root = binding.getRoot();
+        mViewModel = new ViewModelProvider(this).get(BorrarViewModel.class);
+        mViewModel.getToastMessage().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mViewModel.getMutExiste().observe(getViewLifecycleOwner(), new Observer<Inmueble>() {
+            @Override
+            public void onChanged(Inmueble inmueble) {
+                borrarDialog("Desea borrar el inmueble",inmueble);
+
+            }
+        });
+
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mViewModel.
+                mViewModel.buscarInmueble(binding.inputCodigo.getText().toString());
             }
         });
 
@@ -47,6 +69,25 @@ public class BorrarFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(BorrarViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private void borrarDialog(String title, Inmueble inmueble ){
+        new AlertDialog.Builder(getContext())
+                .setTitle(title)
+                .setMessage(inmueble.toString())
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mViewModel.borrarInmueble(inmueble);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .show();
+
     }
 
 }
--- app/src/main/java/com/rafalopez/recuperatorio/ui/borrar/BorrarViewModel.java
+++ app/src/main/java/com/rafalopez/recuperatorio/ui/borrar/BorrarViewModel.java
@@ -1,7 +1,53 @@
 package com.rafalopez.recuperatorio.ui.borrar;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class BorrarViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
+import com.rafalopez.recuperatorio.MainActivity;
+import com.rafalopez.recuperatorio.entity.Inmueble;
+
+public class BorrarViewModel extends AndroidViewModel {
+    private Inmueble inmueble;
+    private  MutableLiveData<String> mToastText;
+    private  MutableLiveData<Inmueble> mExiste;
+
+    public BorrarViewModel(@NonNull Application application) {
+        super(application);
+    }
+    public MutableLiveData<String> getToastMessage() {
+        if(mToastText==null){
+            mToastText=new MutableLiveData<>();
+        }
+        return mToastText;
+    }
+    public MutableLiveData<Inmueble> getMutExiste() {
+        if(mExiste==null){
+            mExiste=new MutableLiveData<>();
+        }
+        return mExiste;
+    }
+
+
+    public  void buscarInmueble(String codigo){
+        for(Inmueble in : MainActivity.inmuebles) {
+            if (in.getCodigo().equalsIgnoreCase(codigo)){
+                mExiste.setValue(in); // tengo que borrar
+                return;
+            }
+        }
+        mToastText.setValue("El Inmueble con ese codigo no existe");
+    }
+    public  void borrarInmueble(Inmueble inmueble){
+        for(Inmueble in : MainActivity.inmuebles) {
+            if (in.equals(inmueble)){
+                MainActivity.inmuebles.remove(in);
+                return;
+            }
+       }
+        mToastText.setValue("El Inmueble se borro");
+    }
+ }
